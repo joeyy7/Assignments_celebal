b013a66e34540492f8d6f6d3dad2f7953f657463
Joel Varghese <joelvarghese356@gmail.com>
2024-05-24 19:49:58 +0530

    completed assignment 2

--- Assignment02.py
+++ Assignment02.py
@@ -1,15 +1,4 @@
 # WAP to Create a simple calculator which can perform simple arithmetic operations like add, subtract, division, multiplication etc
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y 
-
-def divide(x, y):
-    return x / y
 
 while True:
     print("\n----Select operation----")
@@ -21,16 +10,16 @@ while True:
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(num1, "+", num2, "=", num1+num2)
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(num1, "-", num2, "=", num1-num2)
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(num1, "*", num2, "=", num1*num2)
 
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            print(num1, "/", num2, "=", num1/num2)
 
     elif choice == '5':
         print("Exiting...")
